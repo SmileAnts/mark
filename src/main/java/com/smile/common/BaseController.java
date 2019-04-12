d099ea9078435834d8803968b4499f6976a6080f
x2895 <x2895@LAPTOP-S0UT1CJ9>
2019-04-12 15:04:56 +0800

    basecontroller

---
+++ src/main/java/com/smile/common/BaseController.java
@@ -0,0 +1,5 @@
+package com.smile.common;
+
+public class BaseController {
+
+}
---
+++ src/main/java/com/smile/user/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.smile.user.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.smile.common.BaseController;
+
+/**
+ * 用户类 用户得一系列操作
+ * 
+ * @author 许永强
+ *
+ */
+@Controller
+@RequestMapping("/user")
+public class UserController extends BaseController {
+
+}
