061f824a7ae48b60a0b94e0f4dd6e01d62fa3bbf
x2895 <x2895@LAPTOP-S0UT1CJ9>
2019-04-12 13:48:50 +0800

    application 扫描包更改

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication(scanBasePackages = "com.xyqproject.*")
+@SpringBootApplication(scanBasePackages = "com.smile.*")
 @Configuration
 public class Application {
 	public static void main(String[] args) {
