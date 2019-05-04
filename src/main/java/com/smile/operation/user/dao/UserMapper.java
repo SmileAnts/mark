1dd4d361e6735b602259f2841cd026ee3255614f
xuyongqiang <xuyongqiang@git.cloud.tencent.com>
2019-05-04 21:58:24 +0800

    bug

--- src/main/java/com/smile/operation/user/dao/UserMapper.java
+++ src/main/java/com/smile/operation/user/dao/UserMapper.java
@@ -1,7 +1,6 @@
 package com.smile.operation.user.dao;
 
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
 
 import com.baomidou.mybatisplus.mapper.BaseMapper;
 import com.smile.operation.user.entity.User;
@@ -9,7 +8,7 @@ import com.smile.operation.user.entity.User;
 @Mapper
 public interface UserMapper extends BaseMapper<User> {
 
-	public Long login(@Param("user") User user);
+	public Long login(User user);
 
-	public User findByUserName(@Param("username") String username);
+	public User findByUserName(String username);
 }
