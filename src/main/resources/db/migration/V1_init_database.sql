df64653c898999cfed495c6687cf04e48995014a
xuyongqiang <xuyongqiang@git.cloud.tencent.com>
2019-05-21 07:36:35 +0800

    fly wal

---
+++ src/main/resources/db/migration/V1_init_database.sql
@@ -0,0 +1,13 @@
+
+drop table if exists person;
+CREATE TABLE person (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  first varchar(100) NOT NULL,
+  last varchar(100) NOT NULL,
+  dateofbirth DATE DEFAULT null,
+  placeofbirth varchar(100) not null,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+create index index_person_first on person(first);
+insert into person (first,last,dateofbirth,placeofbirth) values('Dursun','KOC', STR_TO_DATE('02/10/1982', '%m/%d/%Y'),'Erzincan');
+insert into person (first,last,dateofbirth,placeofbirth) values('Durseeun','KeeOC', STR_TO_DATE('05/10/1982', '%m/%d/%Y'),'Erzeeincan');
