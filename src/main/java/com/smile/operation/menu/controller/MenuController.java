bd7f31fe645f90bf8ca11e02cc9543c02ac09f97
liuyang <18231195281@163.com>
2019-06-12 18:08:25 +0800

    菜单列表，菜单添加列表

--- src/main/java/com/smile/operation/menu/controller/MenuController.java
+++ src/main/java/com/smile/operation/menu/controller/MenuController.java
@@ -1,8 +1,20 @@
 package com.smile.operation.menu.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.baomidou.mybatisplus.plugins.Page;
+import com.smile.operation.menu.entity.Menu;
+import com.smile.operation.menu.service.IMenuService;
+import com.smile.util.Result;
+import com.smile.util.WrapperUtil;
 
 /**
  * 菜单管理
@@ -13,10 +25,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/menu")
 public class MenuController {
+	@Autowired
+	private IMenuService iMenuServiceImpl;
 
 	@RequestMapping("/index")
 	public String index(Model model) {
 
-		return "html/menu.html";
+		return "menu/menu.html";
+	}
+
+	/**
+	 * 查询所有菜单
+	 */
+	@RequestMapping("/selectAll")
+	@ResponseBody
+	public Object selectAll(Menu menu, HttpServletRequest request, Page<Menu> page) {
+		// page = iMenuServiceImpl.selectPage(page, WrapperUtil.selectList(menu));
+		List<Menu> menus = iMenuServiceImpl.selectList(WrapperUtil.selectList(menu));
+		return Result.success(menus, menus.size());
 	}
 }
